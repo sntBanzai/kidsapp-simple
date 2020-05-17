13a2e59b5fb9fcb2606733af8049f35303fb7da6
Jerzy Małyszko <jerzy.malyszko@wp.pl>
2020-05-17 17:32:17 +0200

    wyszarzanie obrazków

--- app/src/main/java/pl/wroc/waw/kidsapp_simple/DisplayAnimalActivity.java
+++ app/src/main/java/pl/wroc/waw/kidsapp_simple/DisplayAnimalActivity.java
@@ -1,13 +1,29 @@
 package pl.wroc.waw.kidsapp_simple;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class DisplayAnimalActivity extends AppCompatActivity {
+
+    private boolean isGreyScale = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +32,40 @@ public class DisplayAnimalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_display_animal);
         ImageView imgView = findViewById(R.id.imageView);
         int imgToShow = (int) getIntent().getExtras().get("imgToShow");
-        imgView.setImageResource(imgToShow);
+        Bitmap btmp = getImageResourceAsBitmap(imgToShow);
+        imgView.setImageBitmap(btmp);
     }
 
     public void goBack(View v){
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
+    }
+
+    public void fadeInOrOut(View v){
+        isGreyScale = !isGreyScale;
+        ImageView imgView = findViewById(R.id.imageView);
+        int imgToShow = (int) getIntent().getExtras().get("imgToShow");
+        Bitmap btmp = getImageResourceAsBitmap(imgToShow);
+        imgView.setImageBitmap(btmp);
+    }
+
+    public Bitmap getImageResourceAsBitmap(int resourceIdent){
+        Drawable drawable = getResources().getDrawable(resourceIdent);
+
+        Bitmap retVal = ((BitmapDrawable) drawable).getBitmap();
+
+        if(isGreyScale){
+            Bitmap newRetVal = Bitmap.createBitmap(retVal.getWidth(), retVal.getHeight(), Bitmap.Config.ARGB_8888);
+
+            Canvas canvas = new Canvas(newRetVal);
+            Paint paint = new Paint();
+
+            ColorMatrix cm = new ColorMatrix();
+            cm.setSaturation(0);
+            paint.setColorFilter(new ColorMatrixColorFilter(cm));
+            canvas.drawBitmap(retVal, 0, 0, paint);
+            retVal = newRetVal;
+        }
+        return retVal;
     }
 }
